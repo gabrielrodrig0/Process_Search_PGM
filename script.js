2332bb5dbae87428deec3eead8800fecc46f0704
Gabriel Rodrigo Bezerra Araújo <3470648@pgm.rio.rj.gov.br>
2024-03-11 16:55:20 -0300

    Commit inicial

---
+++ script.js
@@ -0,0 +1,18 @@
+function toggleTribunais(label) {
+    var ul = label.nextElementSibling;
+    var span = label.querySelector('span');
+    if (ul.style.display === "none" || ul.style.display === "") {
+        ul.style.display = "block";
+        span.textContent = "▲";
+    } else {
+        ul.style.display = "none";
+        span.textContent = "▼";
+    }
+}
+
+var labels = document.querySelectorAll('label');
+labels.forEach(function(label) {
+    label.addEventListener('click', function() {
+        toggleTribunais(label);
+    });
+});
